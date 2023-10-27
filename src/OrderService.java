f367832c4881423082445d1101da2e18013b391e
Kevin Camacho <Kevincxv@outlook.com>
2023-10-26 22:31:44 -0400

    Updated Implementation

--- src/OrderService.java
+++ src/OrderService.java
@@ -1,19 +1,32 @@
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-
 public class OrderService {
 
     public void placeOrder(int userID, int bookID, int quantity) {
-        String query = "INSERT INTO Orders (userID, bookID, quantity, totalPrice) VALUES (?, ?, ?, calculateTotalPrice(?))";
+        // Fetch the book price first
+        String priceQuery = "SELECT price FROM Books WHERE bookID = ?";
+        BigDecimal bookPrice;
+
         try (Connection conn = DatabaseConnection.getConnection();
-             PreparedStatement stmt = conn.prepareStatement(query)) {
-            stmt.setInt(1, userID);
-            stmt.setInt(2, bookID);
-            stmt.setInt(3, quantity);
-            stmt.setInt(4, bookID);
-            stmt.executeUpdate();
+             PreparedStatement priceStmt = conn.prepareStatement(priceQuery)) {
+            priceStmt.setInt(1, bookID);
+            try (ResultSet rs = priceStmt.executeQuery()) {
+                if (rs.next()) {
+                    bookPrice = rs.getBigDecimal("price");
+                } else {
+                    throw new SQLException("Book not found for given bookID");
+                }
+            }
+
+            BigDecimal totalPrice = bookPrice.multiply(new BigDecimal(quantity));
+
+            String insertQuery = "INSERT INTO Orders (userID, bookID, quantity, totalPrice) VALUES (?, ?, ?, ?)";
+            try (PreparedStatement insertStmt = conn.prepareStatement(insertQuery)) {
+                insertStmt.setInt(1, userID);
+                insertStmt.setInt(2, bookID);
+                insertStmt.setInt(3, quantity);
+                insertStmt.setBigDecimal(4, totalPrice);
+                insertStmt.executeUpdate();
+            }
+
         } catch (SQLException e) {
             e.printStackTrace();
         }
